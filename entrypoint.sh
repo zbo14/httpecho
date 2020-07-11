64394122772e78903e9f37793ea3766be1b676d1
zachary balder <zmbalder@gmail.com>
2020-07-11 17:11:11 -0400

    fix log volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 if [ -z "$1" ]; then
   node index.js > /var/log/httpecho/http.log
 else
-  node index.js -c /etc/httpecho/cert -k /etc/httpecho/private/key -s > /var/log/httpecho/https.log
+  node index.js \
+    -c /etc/httpecho/cert \
+    -k /etc/httpecho/private/key \
+    -s \
+    > /var/log/httpecho/https.log
 fi
